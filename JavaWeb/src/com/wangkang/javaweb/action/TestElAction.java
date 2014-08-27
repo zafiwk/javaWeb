b46162a67f5438fe1a3c9a25c87b1f9ff5de6450
zafiwk <wkdlose@163.com>
2014-08-27 12:08:58 +0800

    V1.3

---
+++ JavaWeb/src/com/wangkang/javaweb/action/TestElAction.java
@@ -0,0 +1,41 @@
+package com.wangkang.javaweb.action;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.wangkang.javaweb.utils.WebUtils;
+
+public class TestElAction extends HttpServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public void destroy() {
+		super.destroy(); 
+	}
+
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+      doPost(request,response);
+	}
+
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		WebUtils  webUtils=WebUtils.getInstance(request, response);
+		request.setAttribute("requestMessgae", "这是request传过来的消息");
+		webUtils.getSession().setAttribute("sessionMessage", "这是session传过来的消息");
+		//RequestDispatcher是针对应用的跳转/指的是根目录
+		webUtils.sendRedirectByreRequest("/test.jsp");
+	}
+
+	public void init() throws ServletException {
+	}
+
+}
---
+++ JavaWeb/src/com/wangkang/javaweb/filter/GzipFilter.java
@@ -0,0 +1,103 @@
+package com.wangkang.javaweb.filter;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+import java.util.zip.GZIPOutputStream;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.WriteListener;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class GzipFilter implements Filter {
+
+	public void destroy() {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void doFilter(ServletRequest req, ServletResponse resp,
+			FilterChain chain) throws IOException, ServletException {
+		final HttpServletRequest request = (HttpServletRequest) req;
+		final HttpServletResponse response = (HttpServletResponse) resp;
+		final ByteArrayOutputStream bout = new ByteArrayOutputStream();
+		final PrintWriter pw = new PrintWriter(new OutputStreamWriter(bout,
+				"UTF-8"));
+
+		// response.getWriter().write("aaa"); responseProxy
+		chain.doFilter(request, (ServletResponse) Proxy.newProxyInstance(
+				GzipFilter.class.getClassLoader(), response.getClass()
+						.getInterfaces(), new InvocationHandler() {
+					public Object invoke(Object proxy, Method method,
+							Object[] args) throws Throwable {
+						if (method.getName().equals("getWriter")) {
+							return pw;
+						} else if (method.getName().equals("getOutputStream")) { // servletOutputStream
+							return new MyServletOutputStream(bout);
+						} else {
+							return method.invoke(response, args);
+						}
+					}
+				}));
+
+		pw.close();
+		byte result[] = bout.toByteArray(); // 拿到目标资源的输出
+		System.out.println("原始大小：" + result.length);
+
+		ByteArrayOutputStream bout2 = new ByteArrayOutputStream();
+		GZIPOutputStream gout = new GZIPOutputStream(bout2);
+		gout.write(result);
+		gout.close();
+
+		byte gzip[] = bout2.toByteArray(); // 拿到目标资源输出的压缩数据
+		System.out.println("压缩大小：" + gzip.length);
+
+		response.setHeader("content-encoding", "gzip");
+		response.setContentLength(gzip.length);
+		response.getOutputStream().write(gzip);
+
+	}
+
+	public void init(FilterConfig filterConfig) throws ServletException {
+		// TODO Auto-generated method stub
+
+	}
+}
+
+class MyServletOutputStream extends ServletOutputStream {
+
+	private ByteArrayOutputStream bout = null;
+
+	public MyServletOutputStream(ByteArrayOutputStream bout) {
+		this.bout = bout;
+	}
+
+	@Override
+	public void write(int b) throws IOException {
+		bout.write(b);
+	}
+
+	@Override
+	public boolean isReady() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void setWriteListener(WriteListener arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
