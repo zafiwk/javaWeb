e6788a38441d634a3950fd6773e54372ea59fdcc
zafiwk <wkdlose@163.com>
2014-10-02 16:48:29 +0800

    添加了分页的样式

--- JavaWeb/src/com/wangkang/javaweb/utils/PageUtils.java
+++ JavaWeb/src/com/wangkang/javaweb/utils/PageUtils.java
@@ -1,6 +1,7 @@
 package com.wangkang.javaweb.utils;
 
 import javax.servlet.http.HttpServletRequest;
+
 import com.wangkang.javaweb.utils.StringUtils;
 
 
@@ -16,17 +17,14 @@ public class PageUtils {
 	private int pageCount = 1;
 	private int currentPage = 1;
 	private int resultStart = -1;
+	
+	private int startPage=1;
+	private int endPage=5;
 	private HttpServletRequest request = null;
 
-	public PageUtils(HttpServletRequest request, int pageSize, int resultCount) {
+	public PageUtils(HttpServletRequest request, int pageSize) {
 		this.request = request;
 		this.pageSize = pageSize;
-		this.resultCount = resultCount;
-		if (resultCount % pageSize == 0)
-			pageCount = resultCount % pageSize;
-		else
-			pageCount = resultCount % pageSize + 1;
-		setResultStart();
 	}
 
 	public int getResultCount() {
@@ -35,6 +33,11 @@ public class PageUtils {
 
 	public void setResultCount(int resultCount) {
 		this.resultCount = resultCount;
+		if (resultCount % pageSize == 0)
+			pageCount = resultCount / pageSize;
+		else
+			pageCount = resultCount / pageSize + 1;
+		setResultStart();
 	}
 
 	public int getPageSize() {
@@ -43,6 +46,11 @@ public class PageUtils {
 
 	public void setPageSize(int pageSize) {
 		this.pageSize = pageSize;
+		if (resultCount % pageSize == 0)
+			pageCount = resultCount / pageSize;
+		else
+			pageCount = resultCount / pageSize + 1;
+		setResultStart();
 	}
 
 	public int getPageCount() {
@@ -69,8 +77,52 @@ public class PageUtils {
 		if(StringUtils.isNotBlank(request.getParameter("page")))
 		{
 			this.currentPage=Integer.valueOf(request.getParameter("page"));
+		    this.startPage=this.startPage+this.currentPage;
+			this.endPage=this.endPage+this.currentPage;
+			
 		}
+		if(this.endPage>this.pageCount)
+		{
+			this.endPage=this.pageCount+1;
+			this.startPage=this.endPage-5;
+		}
+		if(this.startPage<1)
+			this.startPage=1;
 		this.resultStart = (this.currentPage-1)*this.pageSize;
 	}
 
+	public int getStartPage()
+	{
+		return startPage;
+	}
+
+	public int getEndPage()
+	{
+		return endPage;
+	}
+	
+/*	<div class="pager">
+	<ul class="clearfix">
+	<c:if test="${pageUtils.currentPage>1 }">
+	<li><a href="#">上一页</a></li>
+	</c:if>
+	<c:forEach   begin="${pageUtils.startPage }"    end="${pageUtils.endPage }" var="page">
+		<c:if test="${page==pageUtils.currentPage }">
+				<li class="current">${page }</li>
+		</c:if>
+		<c:if test="${page!=pageUtils.currentPage }">
+				<li >${page }</li>
+		</c:if>
+    </c:forEach>
+    <c:if test="${pageUtils.currentPage<pageUtils.pageCount }">
+	<li><a href="#">下一页</a></li>
+	</c:if>
+	</ul>
+</div>
+</div>*/
+/*css
+.pager { margin-top:15px; }
+.pager ul { float:right; }
+.pager ul li { float:left; border:1px solid #eee; line-height:18px; padding:0 3px; margin:0 1px; display:inline; }
+.pager ul li.current { font-weight:bold; color:#630; }*/
 }
